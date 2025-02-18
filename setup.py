eafe2589af267e6b84f679465b6612e3e609747e
luisinhobr <you@example.com>
2025-02-18 14:55:14 -0300

    setup.py!

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zerolm",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "gradio",
+        # Add other dependencies here
+    ],
+)
