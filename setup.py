c471f06493a696a9959cf5d5a7cef9e2a32efa93
luisinhobr <you@example.com>
2025-02-18 14:56:09 -0300

    setup update

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
+# Read requirements from requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="zerolm",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "gradio",
-        # Add other dependencies here
-    ],
+    install_requires=required,
 )
